72d91ac472b9534f5237e7f30471f6f1f920bb52
Bhuvnesh Chaturvedi <93932895+bhuvneshchaturvedi2512@users.noreply.github.com>
2025-06-06 14:19:45 +0530

    Add files via upload

---
+++ CIFAR-10/KnockoffNets/scripts_to_steal_unprotected_model/generate_cifar100_quantized.py
@@ -0,0 +1,178 @@
+import os
+import time
+from functools import partial
+from pathlib import Path
+
+import torch
+from concrete.fhe.compilation.configuration import Configuration
+from models import cnv_2w2a
+from torch.utils.data import DataLoader
+from cifar10_test_quantized import get_train_set_cifar10, get_train_set_cifar100
+
+from concrete import fhe
+from concrete.ml.deployment.fhe_client_server import FHEModelDev
+from concrete.ml.quantization import QuantizedModule
+from concrete.ml.torch.compile import compile_brevitas_qat_model
+from torch.autograd import grad
+import numpy as np
+
+CURRENT_DIR = Path(__file__).resolve().parent
+KEYGEN_CACHE_DIR = CURRENT_DIR.joinpath(".keycache")
+
+# Add MPS (for macOS with Apple Silicon or AMD GPUs) support when error is fixed. For now, we
+# observe a decrease in torch's top1 accuracy when using MPS devices
+# FIXME: https://github.com/zama-ai/concrete-ml-internal/issues/3953
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+def getLabel(expected_quantized_prediction):
+    max_conf = expected_quantized_prediction[0]
+    label = 0
+    for i in range(1,len(expected_quantized_prediction),1):
+        if(expected_quantized_prediction[i] > max_conf):
+            max_conf = expected_quantized_prediction[i]
+            label = i
+    return label
+
+def measure_execution_time(func):
+    """Run a function and return execution time and outputs.
+
+    Usage:
+        def f(x, y):
+            return x + y
+        output, execution_time = measure_execution_time(f)(x,y)
+    """
+
+    def wrapper(*args, **kwargs):
+        # Get the current time
+        start = time.time()
+
+        # Call the function
+        result = func(*args, **kwargs)
+
+        # Get the current time again
+        end = time.time()
+
+        # Calculate the execution time
+        execution_time = end - start
+
+        # Return the result and the execution time
+        return result, execution_time
+
+    return wrapper
+
+
+# Instantiate the model
+torch_model = cnv_2w2a(pre_trained=False)
+torch_model.eval()
+
+
+# Load the saved parameters using the available checkpoint
+checkpoint = torch.load(
+    CURRENT_DIR.joinpath("experiments/CNV_2W2A_2W2A_20231101_103516/checkpoints/best.tar"),
+    map_location=DEVICE,
+)
+torch_model.load_state_dict(checkpoint["state_dict"], strict=False)
+
+# Import and load the CIFAR test dataset
+train_set = get_train_set_cifar10(dataset="CIFAR10", datadir=CURRENT_DIR.joinpath(".datasets/"))
+train_loader = DataLoader(train_set, batch_size=100, shuffle=False)
+
+# Get the first sample
+samples_data = []
+count = 0
+
+for i, data in enumerate(train_loader):
+    samples_data.append(data[0])
+    samples_data.append(data[1])
+    count = count + 1
+    if(count == 1):
+        break
+x, y = samples_data
+
+# Import and load the CIFAR test dataset
+train_set_cifar100 = get_train_set_cifar100(dataset="CIFAR100", datadir=CURRENT_DIR.joinpath(".datasets/"))
+train_loader_cifar100 = DataLoader(train_set_cifar100, batch_size=50000, shuffle=False)
+
+# Get the first sample
+samples_data = []
+count = 0
+
+for i, data in enumerate(train_loader_cifar100):
+    samples_data.append(data[0])
+    samples_data.append(data[1])
+    count = count + 1
+    if(count == 1):
+        break
+x_cifar100, y_cifar100 = samples_data
+
+NUM_SAMPLES = len(x_cifar100)
+print("Length: " + str(NUM_SAMPLES))
+
+
+
+# Parameter `enable_unsafe_features` and `use_insecure_key_cache` are needed in order to be able to
+# cache generated keys through `insecure_key_cache_location`. As the name suggests, these
+# parameters are unsafe and should only be used for debugging in development
+# Multi-parameter strategy is used in order to speed-up the FHE executions
+configuration = Configuration(
+    dump_artifacts_on_unexpected_failures=False,
+    enable_unsafe_features=True,
+    use_insecure_key_cache=True,
+    insecure_key_cache_location=KEYGEN_CACHE_DIR,
+)
+
+print("Compiling the model.")
+quantized_numpy_module, compilation_execution_time = measure_execution_time(
+    compile_brevitas_qat_model
+)(torch_model, x, configuration=configuration, rounding_threshold_bits=6, p_error=0.01)
+assert isinstance(quantized_numpy_module, QuantizedModule)
+
+print(f"Compilation time took {compilation_execution_time} seconds")
+
+# Display the max bit-width in the model
+print(
+    "Max bit-width used in the circuit: ",
+    f"{quantized_numpy_module.fhe_circuit.graph.maximum_integer_bit_width()} bits",
+)
+
+# Save the graph and mlir
+print("Saving graph and mlir to disk.")
+open("cifar10.graph", "w").write(str(quantized_numpy_module.fhe_circuit))
+open("cifar10.mlir", "w").write(quantized_numpy_module.fhe_circuit.mlir)
+
+# Data torch to numpy
+x_numpy = x_cifar100.numpy()
+
+for image_index in range(NUM_SAMPLES):
+    test_x_numpy = x_numpy[image_index : image_index + 1]
+    
+    q_x_numpy, quantization_execution_time = measure_execution_time(
+        quantized_numpy_module.quantize_input
+    )(test_x_numpy)
+    
+    for i in range(len(q_x_numpy[0])):
+        for j in range(len(q_x_numpy[0][0])):
+            for k in range(len(q_x_numpy[0][0][0])):
+                with open('X_cifar100_test.txt','a') as fp:
+                    fp.write(str(test_x_numpy[0][i][j][k]) + ",")
+            with open('X_cifar100_test.txt','a') as fp:
+                fp.write("#")
+        with open('X_cifar100_test.txt','a') as fp:
+            fp.write("|")
+    with open('X_cifar100_test.txt','a') as fp:
+        fp.write("\n")
+    
+    p_error = quantized_numpy_module.fhe_circuit.p_error
+    expected_quantized_prediction, clear_inference_time = measure_execution_time(
+        partial(quantized_numpy_module.fhe_circuit.graph, p_error=p_error)
+    )(q_x_numpy)
+
+    predicted_label = getLabel(expected_quantized_prediction[0])
+    
+    with open('Y_cifar100_test.txt','a') as fp:
+        fp.write(str(predicted_label) + '\n')  
+    
+    if(image_index % 100 == 0):
+        print("First " + str(image_index) + " samples written")
+  
+print("Samples generated")
---
+++ CIFAR-10/KnockoffNets/scripts_to_steal_unprotected_model/trainer_stolen_densenet.py
@@ -0,0 +1,134 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.applications import DenseNet121
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler
+import numpy as np
+
+# Learning rate scheduler
+def lr_schedule(epoch):
+    learning_rate = 1e-3
+    if epoch > 40:
+        learning_rate *= 1e-2
+    elif epoch > 30:
+        learning_rate *= 1e-1
+    return learning_rate
+
+
+x_cifar10 = []
+y_cifar10 = []
+
+with open('X_cifar10_test.txt','r') as fp:
+	lines_x = fp.readlines()
+	
+with open('Y_cifar10_test.txt','r') as fp:
+	lines_y = fp.readlines()
+	
+
+for line_x in lines_x:
+	temp_channel = line_x.strip().split('|')
+	list_x_channel = []
+	for channel in temp_channel:
+		if(channel != ''):
+			temp_row = channel.strip().split('#')
+			list_x_row = []
+			for row in temp_row:
+				if(row != ''):
+					temp_column = row.strip().split(',')
+					list_x_column = []
+					for column in temp_column:
+						if(column != ''):
+							list_x_column.append(float(column))
+					list_x_row.append(np.array(list_x_column, dtype=np.float32))
+			list_x_channel.append(list(list_x_row))
+	x_cifar10.append(np.array(list_x_channel, dtype=np.float32))
+
+for line_y in lines_y:
+	temp = line_y.strip()
+	if(temp != ''):
+		y_cifar10.append(int(temp))
+
+y_cifar10 = to_categorical(y_cifar10, 10)
+
+x_cifar10_new = []
+for i in range(len(lines_y)):
+	x_cifar10_new.append(np.transpose(x_cifar10[i],(1, 2, 0)).tolist())
+
+x_cifar100 = []
+y_cifar100 = []
+
+with open('X_cifar100_test.txt','r') as fp:
+	lines_x = fp.readlines()
+	
+with open('Y_cifar100_test.txt','r') as fp:
+	lines_y = fp.readlines()
+	
+
+for line_x in lines_x:
+	temp_channel = line_x.strip().split('|')
+	list_x_channel = []
+	for channel in temp_channel:
+		if(channel != ''):
+			temp_row = channel.strip().split('#')
+			list_x_row = []
+			for row in temp_row:
+				if(row != ''):
+					temp_column = row.strip().split(',')
+					list_x_column = []
+					for column in temp_column:
+						if(column != ''):
+							list_x_column.append(float(column))
+					list_x_row.append(np.array(list_x_column, dtype=np.float32))
+			list_x_channel.append(list(list_x_row))
+	x_cifar100.append(np.array(list_x_channel, dtype=np.float32))
+
+for line_y in lines_y:
+	temp = line_y.strip()
+	if(temp != ''):
+		y_cifar100.append(int(temp))
+
+y_cifar100 = to_categorical(y_cifar100, 10)
+
+x_cifar100_new = []
+for i in range(len(lines_y)):
+	x_cifar100_new.append(np.transpose(x_cifar100[i],(1, 2, 0)).tolist())
+
+x_cifar10_new = np.array(x_cifar10_new)
+y_cifar10 = np.array(y_cifar10)
+
+x_cifar100_new = np.array(x_cifar100_new)
+y_cifar100 = np.array(y_cifar100)
+
+
+
+base_model = DenseNet121(weights='imagenet', include_top=False)
+
+# Add custom layers on top of DenseNet
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(1024, activation='relu')(x)
+predictions = Dense(10, activation='softmax')(x)
+
+# Create final model
+model = Model(inputs=base_model.input, outputs=predictions)
+
+lr_scheduler = LearningRateScheduler(lr_schedule)
+
+checkpoint = ModelCheckpoint("stolen_model.h5", monitor='val_loss', verbose = 1, save_best_only = True, mode='min')
+callbacks = [checkpoint, lr_scheduler]
+
+#model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=lr_schedule(0)), metrics=['accuracy'])
+
+with tf.device('/device:GPU:0'): # /device:GPU:0 for training in GPU
+	model.fit(x_cifar100_new, y_cifar100, epochs=100, batch_size=64, verbose=1, validation_data=(x_cifar10_new, y_cifar10), callbacks=callbacks)
+
+#model_save_path = "/home/seal-lab-workstation/space_submission_2023/Model_Stealing_in_FHE/concrete_cifar_brevitas_training/KnockoffNets/CIFAR100_with_quantized_datapoints/stolen_model.h5"
+model = load_model("stolen_model.h5")
+
+_, accuracy = model.evaluate(x_cifar10_new, y_cifar10)
+print(f"Accuracy of stolen model: {np.around(accuracy*100, 2)}")
