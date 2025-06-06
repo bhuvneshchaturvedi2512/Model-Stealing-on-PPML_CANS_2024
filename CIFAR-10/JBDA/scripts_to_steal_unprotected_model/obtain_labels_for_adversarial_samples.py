11503953a7d9821526024aece2a2854b8e6941ab
Bhuvnesh Chaturvedi <93932895+bhuvneshchaturvedi2512@users.noreply.github.com>
2025-06-06 14:08:29 +0530

    Add files via upload

---
+++ CIFAR-10/JBDA/scripts_to_steal_unprotected_model/obtain_labels_for_adversarial_samples.py
@@ -0,0 +1,164 @@
+import os
+import time
+from functools import partial
+from pathlib import Path
+
+import torch
+from concrete.fhe.compilation.configuration import Configuration
+from models import cnv_2w2a
+from torch.utils.data import DataLoader
+from trainer_surrogate import get_train_set_cifar10
+
+from concrete import fhe
+from concrete.ml.deployment.fhe_client_server import FHEModelDev
+from concrete.ml.quantization import QuantizedModule
+from concrete.ml.torch.compile import compile_brevitas_qat_model
+
+CURRENT_DIR = Path(__file__).resolve().parent
+KEYGEN_CACHE_DIR = CURRENT_DIR.joinpath(".keycache")
+
+# Add MPS (for macOS with Apple Silicon or AMD GPUs) support when error is fixed. For now, we
+# observe a decrease in torch's top1 accuracy when using MPS devices
+# FIXME: https://github.com/zama-ai/concrete-ml-internal/issues/3953
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+NUM_SAMPLES = 1000
+
+def getLabel(expected_quantized_prediction):
+    max_conf = expected_quantized_prediction[0]
+    label = 0
+    for i in range(1,len(expected_quantized_prediction),1):
+        if(expected_quantized_prediction[i] > max_conf):
+            max_conf = expected_quantized_prediction[i]
+            label = i
+    return label
+
+def measure_execution_time(func):
+    """Run a function and return execution time and outputs.
+
+    Usage:
+        def f(x, y):
+            return x + y
+        output, execution_time = measure_execution_time(f)(x,y)
+    """
+
+    def wrapper(*args, **kwargs):
+        # Get the current time
+        start = time.time()
+
+        # Call the function
+        result = func(*args, **kwargs)
+
+        # Get the current time again
+        end = time.time()
+
+        # Calculate the execution time
+        execution_time = end - start
+
+        # Return the result and the execution time
+        return result, execution_time
+
+    return wrapper
+
+
+# Instantiate the model
+torch_model = cnv_2w2a(pre_trained=False)
+torch_model.eval()
+
+# Load the saved parameters using the available checkpoint
+checkpoint = torch.load(
+    CURRENT_DIR.joinpath("experiments/CNV_2W2A_2W2A_20231101_103516/checkpoints/best.tar"),
+    map_location=DEVICE,
+)
+
+torch_model.load_state_dict(checkpoint["state_dict"], strict=False)
+
+# Import and load the CIFAR test dataset
+test_set = get_train_set_cifar10(dataset="CIFAR10", datadir=CURRENT_DIR.joinpath(".datasets/"))
+test_loader = DataLoader(test_set, batch_size=1000, shuffle=False)
+
+# Get the first sample
+samples_data = []
+count = 0
+
+for i, data in enumerate(test_loader):
+    samples_data.append(data[0])
+    samples_data.append(data[1])
+    count = count + 1
+    if(count == 1):
+        break
+x, y = samples_data
+
+# Parameter `enable_unsafe_features` and `use_insecure_key_cache` are needed in order to be able to
+# cache generated keys through `insecure_key_cache_location`. As the name suggests, these
+# parameters are unsafe and should only be used for debugging in development
+# Multi-parameter strategy is used in order to speed-up the FHE executions
+configuration = Configuration(
+    dump_artifacts_on_unexpected_failures=False,
+    enable_unsafe_features=True,
+    use_insecure_key_cache=True,
+    insecure_key_cache_location=KEYGEN_CACHE_DIR,
+)
+
+print("Compiling the model.")
+quantized_numpy_module, compilation_execution_time = measure_execution_time(
+    compile_brevitas_qat_model
+)(torch_model, x, configuration=configuration, rounding_threshold_bits=6, p_error=0.01)
+assert isinstance(quantized_numpy_module, QuantizedModule)
+
+print(f"Compilation time took {compilation_execution_time} seconds")
+
+# Display the max bit-width in the model
+print(
+    "Max bit-width used in the circuit: ",
+    f"{quantized_numpy_module.fhe_circuit.graph.maximum_integer_bit_width()} bits",
+)
+
+# Save the graph and mlir
+print("Saving graph and mlir to disk.")
+open("cifar10.graph", "w").write(str(quantized_numpy_module.fhe_circuit))
+open("cifar10.mlir", "w").write(quantized_numpy_module.fhe_circuit.mlir)
+
+#augmenting adversarial samples with perturbation 0.5
+x_test_adversarial_samples = []
+y_test_adversarial_samples = []
+        
+with open('X_adversarial_samples_0_5.txt','r') as fp:
+	lines_x = fp.readlines()
+   
+for line_x in lines_x:
+	temp_channel = line_x.strip().split('|')
+	list_x_channel = []
+	for channel in temp_channel:
+		if(channel != ''):
+			temp_row = channel.strip().split('#')
+			list_x_row = []
+			for row in temp_row:
+				if(row != ''):
+					temp_column = row.strip().split(',')
+					list_x_column = []
+					for column in temp_column:
+						if(column != ''):
+							list_x_column.append(float(column))
+					list_x_row.append(list(list_x_column))
+			list_x_channel.append(list(list_x_row))
+	x_test_adversarial_samples.append(list(list_x_channel))
+	
+for image_index in range(NUM_SAMPLES):
+	# Take one example
+	test_x_numpy = x_test_adversarial_samples[image_index : image_index + 1]
+	# Quantize the input
+	q_x_numpy, quantization_execution_time = measure_execution_time(
+	quantized_numpy_module.quantize_input
+	)(test_x_numpy)
+
+	p_error = quantized_numpy_module.fhe_circuit.p_error
+	expected_quantized_prediction, clear_inference_time = measure_execution_time(
+	partial(quantized_numpy_module.fhe_circuit.graph, p_error=p_error)
+	)(q_x_numpy)
+
+	predicted_label = getLabel(expected_quantized_prediction[0])
+    
+	with open('Y_adversarial_samples_0_5.txt','a') as fp:
+		fp.write(str(predicted_label) + '\n')
+        
+print("Predictions obtained for adversarial samples")
